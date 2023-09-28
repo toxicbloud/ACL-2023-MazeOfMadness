bda4154b2cefc81a07f4f2949097b5391c7872c6
FurWaz <fur.waz06@gmail.com>
2023-09-28 09:39:14 +0200

    Fixed engine checkstyle

--- mom/src/main/java/com/engine/Evolvable.java
+++ mom/src/main/java/com/engine/Evolvable.java
@@ -1,6 +1,19 @@
 package com.engine;
 
+/**
+ * Evolvable interface.
+ * This is the interface for all evolvable objects.
+ */
 public interface Evolvable {
-    public void update();
-    public void render();
+    /**
+     * Update the evolvable object.
+     * This is called every frame before any render() call.
+     */
+    void update();
+
+    /**
+     * Render the evolvable object.
+     * This is called every frame after all update() calls.
+     */
+    void render();
 }
--- mom/src/main/java/com/engine/utils/Time.java
+++ mom/src/main/java/com/engine/utils/Time.java
@@ -1,21 +1,27 @@
 package com.engine.utils;
 
-public class Time {
-    private static Time Instance = null;
+/**
+ * Time class.
+ * This class is used to get the time.
+ */
+public final class Time {
+    /** Number of milliseconds in a second. */
+    private static final float NB_MILLIS_IN_SECOND = 1000.0f;
+    /** Time instanced class. */
+    private static Time instance;
 
-    public static Time GetInstance() {
-        if (Time.Instance == null)
-            Time.Instance = new Time();
-        return Time.Instance;
-    }
-
+    /** time at app launch. */
     private float launchTime;
+    /** current app time. */
     private float currentTime;
+    /** last frame app time. */
     private float lastTime;
+    /** delta time between now and last frame. */
     private float deltaTime;
 
-    private float millis2seconds(long millis) { return millis / 1000.0f; }
-
+    /**
+     * Time constructor.
+     */
     private Time() {
         launchTime = millis2seconds(System.currentTimeMillis());
         currentTime = launchTime;
@@ -23,14 +29,47 @@ public class Time {
         deltaTime = 0.0f;
     }
 
+    /**
+     * Convert milliseconds to seconds.
+     * @param millis The milliseconds.
+     * @return The seconds.
+     */
+    private float millis2seconds(long millis) {
+        return millis / NB_MILLIS_IN_SECOND;
+    }
+
+    /**
+     * Get the time instance.
+     * @return The time instance.
+     */
+    public static Time getInstance() {
+        if (Time.instance == null) {
+            Time.instance = new Time();
+        }
+        return Time.instance;
+    }
+
+    /**
+     * Get the absolute time since app launch.
+     * @return The absolute time since app launch.
+     */
     public float getAbsTime() {
         return currentTime - launchTime;
     }
 
+    /**
+     * Get the delta time between now and last frame.
+     * @return The delta time between now and last frame.
+     */
     public float getDeltaTime() {
         return deltaTime;
     }
 
+    /**
+     * Update the time.
+     * Calculate the delta time between now and last frame.
+     * (Do before each frame cycle)
+     */
     public void update() {
         lastTime = currentTime;
         currentTime = millis2seconds(System.currentTimeMillis());
