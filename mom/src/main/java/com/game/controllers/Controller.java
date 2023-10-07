36782a160e7ce194e53fd53c7707cc5a8f795b50
FurWaz <fur.waz06@gmail.com>
2023-10-07 13:11:26 +0200

    Started player controller setup

---
+++ mom/src/main/java/com/game/controllers/Controller.java
@@ -0,0 +1,27 @@
+package com.game.controllers;
+
+import com.game.Entity;
+
+/**
+ * Controller class.
+ * This is the base class for all controllers.
+ */
+public abstract class Controller {
+    /** Controller's target. */
+    private Entity target;
+
+    /**
+     * Controller constructor.
+     * @param target The target of the controller.
+     */
+    public Controller(Entity target) {
+        this.target = target;
+        this.target.registerController(this);
+    }
+
+    /**
+     * Update the controller.
+     * This method is called every frame, before any render() call.
+     */
+    public abstract void update();
+}
---
+++ mom/src/main/java/com/game/controllers/PlayerController.java
@@ -0,0 +1,22 @@
+package com.game.controllers;
+
+import com.game.Player;
+
+/**
+ * PlayerController class.
+ * This is the player controller class.
+ */
+public class PlayerController extends Controller {
+    /**
+     * PlayerController constructor.
+     * @param player The player to control.
+     */
+    public PlayerController(Player player) {
+        super(player);
+    }
+
+    @Override
+    public void update() {
+        // TODO
+    }
+}
