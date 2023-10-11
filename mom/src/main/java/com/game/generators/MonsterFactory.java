13d1a2165faaaed800d47eb3c72dc171c9607d51
Maxime Marcelin <70325779+Maximelego@users.noreply.github.com>
2023-10-11 10:57:31 +0200

    Added MonsterFactory static Methods.

--- mom/src/main/java/com/game/generators/MonsterFactory.java
+++ mom/src/main/java/com/game/generators/MonsterFactory.java
@@ -4,5 +4,26 @@ package com.game.generators;
  * Monster Factory class.
  * Creates monsters to place on the maze.
  */
-public class MonsterFactory {
+public final class MonsterFactory {
+
+    /**
+     * MonsterFactory constructor.
+     */
+    private MonsterFactory() {}
+
+    /**
+     * createZombie function. Creates a Zombie Mob.
+     */
+    public static void createZombie() {}
+
+    /**
+     * createGhost function. Creates a Ghost Mob.
+     */
+    public static void createGhost() {}
+
+    /**
+     * createBoss function. Creates a Boss Mob.
+     */
+    public static void createBoss() {}
+
 }
