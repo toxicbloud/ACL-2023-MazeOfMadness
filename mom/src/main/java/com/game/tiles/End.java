e3d760d002731e7e2e94671dcadea3c25fb07e75
Antonin Rousseau <fanta.r55000@gmail.com>
2023-10-30 15:05:22 +0100

    Add tile to finish the game WIP
    @FurWaz need your help to understand how to render properly the ground below it

---
+++ mom/src/main/java/com/game/tiles/End.java
@@ -0,0 +1,48 @@
+package com.game.tiles;
+
+import com.engine.Sprite;
+import com.engine.Window;
+import com.game.Player;
+import com.ui.EndScene;
+
+/**
+ * End class.
+ * Finish the game when the player reach this tile.
+ */
+public class End extends Ground {
+
+    /**
+     * End sprite shift from texture top.
+     */
+    private static final int END_SPRITE_SHIFT = SPRITE_SIZE * 3;
+
+    /**
+     * Ground below the chest.
+     */
+    private final Sprite ground;
+
+    /**
+     * End constructor.
+     *
+     *
+     */
+    public End() {
+        super(TileType.GROUND_END, new Sprite(TILE_TEXTURE, SPRITE_SIZE, SPRITE_SIZE, END_SPRITE_SHIFT));
+        ground = new Sprite(TILE_TEXTURE, SPRITE_SIZE, SPRITE_SIZE, 1);
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void onPlayerExit(Player p) {
+    }
+
+    @Override
+    public void onPlayerEnter(Player p) {
+        Window.getInstance().setScene(new EndScene(true));
+    }
+
+}
