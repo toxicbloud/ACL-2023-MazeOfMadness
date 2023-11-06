7ebc0d28de9fdd9ec3a35e6f244a0183a2a1121d
Maxime Marcelin <maisonmarc7@gmail.com>
2023-11-06 19:45:53 +0100

    Added a static TrapSpawner class.

---
+++ mom/src/main/java/com/game/generators/TrapSpawner.java
@@ -0,0 +1,21 @@
+package com.game.generators;
+
+import com.game.Maze;
+
+/**
+ * Trap spawner class.
+ */
+public final class TrapSpawner {
+
+    /** Private constructor for the TrapSpawner class. */
+    private TrapSpawner() {}
+
+    /**
+     * This Method spawn different traps inside the maze.
+     *
+     * @param maze Maze to populate.
+     */
+    public static void spawnTraps(Maze maze) {
+
+    }
+}
