5cde3dc98b96b56737ffbbe4b3ba6c37fc6b5e25
FurWaz <fur.waz06@gmail.com>
2023-11-08 15:39:30 +0100

    Fixed only 1 monsters in map

--- mom/src/main/java/com/game/generators/MonsterSpawner.java
+++ mom/src/main/java/com/game/generators/MonsterSpawner.java
@@ -47,8 +47,6 @@ public final class MonsterSpawner {
                     Zombie zombie = new Zombie(new Vector3(x, y, 1));
                     new ZombieController(zombie);
                     monsters.add(zombie);
-                    maze.setMonsters(monsters.toArray(new Monster[monsters.size()]));
-                    return;
                 }
             }
         }
