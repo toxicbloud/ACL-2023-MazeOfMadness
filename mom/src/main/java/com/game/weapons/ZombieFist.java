289240e6048e70173f48b594d5f13deafe5b3cd5
Clement.JL <118436575+aildontknow@users.noreply.github.com>
2023-11-20 23:28:34 +0100

    Create ZombieFist.java

---
+++ mom/src/main/java/com/game/weapons/ZombieFist.java
@@ -0,0 +1,19 @@
+package com.game.weapons;
+
+/**
+ * ZombieFist class.
+ * This is the zombie fist class.
+ */
+public class ZombieFist extends WeaponFist {
+    /** ZombieFist damage amout. */
+    private static final int DAMAGE = 5;
+    /** ZombieFist cooldown. */
+    private static final int ATTACK_COOLDOWN = 500;
+
+    /**
+     * ZombieFist constructor.
+     */
+    public ZombieFist() {
+        super(ZombieFist.DAMAGE, ZombieFist.ATTACK_COOLDOWN);
+    }
+}
