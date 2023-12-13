58827451727604aef17215bb180db17ccdaff215
Clement.JL <118436575+aildontknow@users.noreply.github.com>
2023-12-13 17:27:32 +0100

    Adding different sounds to the manager

--- mom/src/main/java/com/engine/SoundManager.java
+++ mom/src/main/java/com/engine/SoundManager.java
@@ -8,19 +8,59 @@ import com.badlogic.gdx.backends.lwjgl3.audio.Mp3.Sound;
  * It is the manager of all sounds in the game.
  */
 public final class SoundManager {
+    /** Set the player's damage sound volume. */
+    private static final float PLAYER_DAMAGE_VOLUME = 0.3f;
+    /** Set the zombie's attack sound volume. */
+    private static final float ZOMBIE_ATTACK_VOLUME = 0.3f;
+    /** Set the zombie's damage sound volume. */
+    private static final float ZOMBIE_DAMAGE_VOLUME = 0.25f;
+    /** Set the potion sound volume. */
+    private static final float POTION_VOLUME = 0.25f;
     /** SoundManager instanced class. */
     private static SoundManager instance;
-    /** Attack sound. */
-    private Sound attackSound;
+    /** Player attack sound. */
+    private Sound playerAttackSound;
+    /** Player damage sound. */
+    private Sound playerDamageSound;
+    /** Zombie attack sound. */
+    private Sound zombieAttackSound;
+    /** Zombie damage sound. */
+    private Sound zombieDamageSound;
+    /** Zombie ambient sound. */
+    private Sound zombieAmbientSound;
     /** Potion sound. */
     private Sound potionSound;
+    /** Sound when walking on the floor. */
+    private Sound footStepGround;
+    /** Sound when walking in lava. */
+    private Sound footStepLava;
+    /** Sound when walking in water. */
+    private Sound footStepWater;
+    /** The button click sound. */
+    private Sound buttonClick;
 
     /** Sound enum. */
     public enum SoundList {
-        /** Attack sound. */
-        ATTACK,
-        /** Sound of drinking a potion. */
-        POTION
+        /** Player attack sound. */
+        PLAYER_ATTACK,
+        /** Player damage sound. */
+        PLAYER_DAMAGE,
+        /** Zombie attack sound. */
+        ZOMBIE_ATTACK,
+        /** Zombie damage sound. */
+        ZOMBIE_DAMAGE,
+        /** Zombie ambient sound. */
+        ZOMBIE_AMBIENT,
+        /** Potion sound. */
+        POTION,
+        /** Sound when walking on the floor. */
+        FOOTSTEP_GROUND,
+        /** Sound when walking in lava. */
+        FOOTSTEP_LAVA,
+        /** Sound when walking in water. */
+        FOOTSTEP_WATER,
+        /** The button click sound. */
+        BUTTON_CLICK
     }
 
     private SoundManager() {
@@ -40,8 +80,16 @@ public final class SoundManager {
     }
 
     private void init() {
-        attackSound = (Sound) Gdx.audio.newSound(Gdx.files.internal("sounds/punch.mp3"));
+        playerAttackSound = (Sound) Gdx.audio.newSound(Gdx.files.internal("sounds/playerAttack.mp3"));
+        playerDamageSound = (Sound) Gdx.audio.newSound(Gdx.files.internal("sounds/playerDamage.mp3"));
+        zombieAttackSound = (Sound) Gdx.audio.newSound(Gdx.files.internal("sounds/zombieAttack.mp3"));
+        zombieDamageSound = (Sound) Gdx.audio.newSound(Gdx.files.internal("sounds/zombieDamage.mp3"));
+        zombieAmbientSound = (Sound) Gdx.audio.newSound(Gdx.files.internal("sounds/zombieAmbient.mp3"));
         potionSound = (Sound) Gdx.audio.newSound(Gdx.files.internal("sounds/potion.mp3"));
+        footStepGround = (Sound) Gdx.audio.newSound(Gdx.files.internal("sounds/footstepGround.mp3"));
+        footStepLava = (Sound) Gdx.audio.newSound(Gdx.files.internal("sounds/potion.mp3"));
+        footStepWater = (Sound) Gdx.audio.newSound(Gdx.files.internal("sounds/footstepWater.mp3"));
+        buttonClick = (Sound) Gdx.audio.newSound(Gdx.files.internal("sounds/punch.mp3"));
     }
 
     /**
@@ -51,11 +99,63 @@ public final class SoundManager {
      */
     public void play(SoundList soundList) {
         switch (soundList) {
-            case ATTACK:
-                attackSound.play();
+            case PLAYER_ATTACK:
+                playerAttackSound.play();
+                break;
+            case PLAYER_DAMAGE:
+                long playerDamage = playerDamageSound.play();
+                playerDamageSound.setVolume(playerDamage, PLAYER_DAMAGE_VOLUME);
+                break;
+            case ZOMBIE_ATTACK:
+                long zombieAttack = zombieAttackSound.play();
+                zombieAttackSound.setVolume(zombieAttack, ZOMBIE_ATTACK_VOLUME);
+                break;
+            case ZOMBIE_DAMAGE:
+                long zombieDamage = zombieDamageSound.play();
+                zombieDamageSound.setVolume(zombieDamage, ZOMBIE_DAMAGE_VOLUME);
+                break;
+            case ZOMBIE_AMBIENT:
+                zombieAmbientSound.loop();
                 break;
             case POTION:
-                potionSound.play();
+                long potion = potionSound.play();
+                potionSound.setVolume(potion, POTION_VOLUME);
+                break;
+            case FOOTSTEP_GROUND:
+                footStepGround.loop();
+                break;
+            case FOOTSTEP_LAVA:
+                footStepLava.loop();
+                break;
+            case FOOTSTEP_WATER:
+                footStepWater.loop();
+                break;
+            case BUTTON_CLICK:
+                buttonClick.play();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Stop the right sound.
+     *
+     * @param soundList Enumerating sounds.
+     */
+    public void stop(SoundList soundList) {
+        switch (soundList) {
+            case ZOMBIE_AMBIENT:
+                zombieAmbientSound.stop();
+                break;
+            case FOOTSTEP_GROUND:
+                footStepGround.stop();
+                break;
+            case FOOTSTEP_LAVA:
+                footStepLava.stop();
+                break;
+            case FOOTSTEP_WATER:
+                footStepWater.stop();
                 break;
             default:
                 break;
